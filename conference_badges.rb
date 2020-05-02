bbd80006d3aa450bdd6ffb8ee16b42e39a3255d3
Spencer Cambern <cambern134@gmail.com>
2020-05-02 04:22:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
   end
-assign_rooms(attendees).each do |assignments|
+assign_rooms(attendees).each do |assignment|
   puts assignment
  end
 end
